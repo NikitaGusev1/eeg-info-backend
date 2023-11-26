92310fdec9de43740b6c9f89ad53c04a1d3f8668
Nikita <nkgs370@gmail.com>
2023-11-26 13:36:10 +0200

    better algo

--- scripts/find_peaks.py
+++ scripts/find_peaks.py
@@ -2,72 +2,65 @@ import json
 import sys
 import numpy as np
 from scipy.ndimage import grey_erosion, grey_dilation
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks, firwin, lfilter
+
+def apply_structuring_element(signal, a, b):
+    signal_array = np.array(signal)
+    return a * signal_array**2 + b
+
+def apply_fir_filter(signal, sampling_frequency, cutoff_frequency=30.0):
+    nyquist = 0.5 * sampling_frequency
+    num_taps = 101  # Adjust the number of taps as needed
+    cutoff = cutoff_frequency / nyquist
+    fir_coefficients = firwin(num_taps, cutoff, window='hamming')
+    filtered_signal = lfilter(fir_coefficients, 1.0, signal)
+    return filtered_signal
+
+def calculate_threshold(signal):
+    # Find the indices of extrema (peaks and troughs)
+    diff_signal = np.diff(signal)
+    extrema_up = np.where(diff_signal > 0)[0] + 1  # Add 1 to get the correct index for peaks
+    extrema_down = np.where(diff_signal < 0)[0] + 1  # Add 1 to get the correct index for troughs
+    extrema = np.concatenate((extrema_up, extrema_down))
+
+    # Ensure extrema indices are within the valid range
+    extrema = extrema[(extrema > 0) & (extrema < len(signal) - 1)]
+
+    # Calculate the average value of the absolute extrema
+    avg_extrema = np.mean(np.abs(signal[extrema]))
+
+    # Calculate the threshold as 8 times the average of the absolute extrema
+    threshold = 8 * avg_extrema
+
+    return threshold
 
 def detect_peaks(eeg_signal, sampling_frequency, duration_minutes=1, open_size=5, close_size=5):
-    def grey_opening(signal):
-        eroded_signal = grey_erosion(signal, size=(open_size,))
-        opened_signal = grey_dilation(eroded_signal, size=(open_size,))
-        return opened_signal
+    # Constants for structuring elements
+    a1, b1 = 1.0, 1.0
+    a2, b2 = 1.0, 1.0
 
-    def grey_closing(signal):
-        dilated_signal = grey_dilation(signal, size=(close_size,))
-        closed_signal = grey_erosion(dilated_signal, size=(close_size,))
-        return closed_signal
+    # Apply the structuring elements to the original signal
+    g1_signal = apply_structuring_element(eeg_signal, a1, b1)
+    g2_signal = apply_structuring_element(eeg_signal, a2, b2)
 
-    def close_opening(signal):
-        closed_signal = grey_closing(signal)
-        opened_closed_signal = grey_opening(closed_signal)
-        return opened_closed_signal
+    # Apply FIR filter to the signals
+    g1_signal_filtered = apply_fir_filter(g1_signal, sampling_frequency)
+    g2_signal_filtered = apply_fir_filter(g2_signal, sampling_frequency)
 
-    def open_closing(signal):
-        opened_signal = grey_opening(signal)
-        closed_opened_signal = grey_closing(opened_signal)
-        return closed_opened_signal
+    # Apply opening and closing operations to the filtered signals
+    opened_g1_signal = grey_dilation(grey_erosion(g1_signal_filtered, size=(open_size,)), size=(open_size,))
+    closed_g2_signal = grey_erosion(grey_dilation(g2_signal_filtered, size=(close_size,)), size=(close_size,))
 
-    def average_of_operators(signal, operator1, operator2):
-        result1 = operator1(signal)
-        result2 = operator2(signal)
-        averaged_signal = (result1 + result2) / 2.0
-        return averaged_signal
-
-    def calculate_threshold(signal):
-        # Find the indices of extrema (peaks and troughs)
-        diff_signal = np.diff(signal)
-        extrema_up = np.where(diff_signal > 0)[0] + 1  # Add 1 to get the correct index for peaks
-        extrema_down = np.where(diff_signal < 0)[0] + 1  # Add 1 to get the correct index for troughs
-        extrema = np.concatenate((extrema_up, extrema_down))
-
-        # Ensure extrema indices are within the valid range
-        extrema = extrema[(extrema > 0) & (extrema < len(signal) - 1)]
-
-        # Calculate the threshold as 8 times the median of the absolute values of the extrema
-        threshold = 8 * np.median(np.abs(signal[extrema]))
-
-        return threshold
-
-    # Ensure that eeg_signal is a one-dimensional NumPy array
-    eeg_signal = np.asarray(eeg_signal).ravel()
-
-    # Calculate the number of samples corresponding to the specified duration
-    window_samples = int(duration_minutes * 60 * sampling_frequency)
-
-    # Trim the signal to the specified duration
-    eeg_signal = eeg_signal[:window_samples]
-
-    # Calculate the threshold based on the formula
-    threshold = calculate_threshold(eeg_signal)
-
-    # Apply the opening and closing operators
-    filtered_signal = average_of_operators(eeg_signal, close_opening, open_closing)
+    # Calculate the average of the two signals
+    averaged_signal = (opened_g1_signal + closed_g2_signal) / 2.0
 
     # Find peaks using the calculated threshold
-    peaks, _ = find_peaks(filtered_signal, height=threshold)
+    peaks, _ = find_peaks(averaged_signal, height=calculate_threshold(averaged_signal))
     peaks_count = len(peaks)
 
     result = {
         "peaks_count": peaks_count,
-        "threshold_used": float(threshold),
+        "debug_info": {}  # Add any additional debug information as needed
     }
 
     return result
