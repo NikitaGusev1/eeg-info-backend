c8b5c2894a51c2084deca09b60adce64984d9c64
Nikita <nkgs370@gmail.com>
2023-12-01 00:22:32 +0200

    wip

--- scripts/find_peaks.py
+++ scripts/find_peaks.py
@@ -1,30 +1,31 @@
 import json
 import sys
 import numpy as np
-from scipy.signal import find_peaks, butter, filtfilt
+from scipy.signal import find_peaks
 from scipy.ndimage import grey_erosion, grey_dilation
 
-def detect_eeg_peaks(signals, start_minute=4, duration_minutes=1):
-    # Define a function for opening operation
+def detect_eeg_peaks(signals, start_minute=0, duration_minutes=1):
+    def trim_signal(signal, sampling_frequency, start_minute, duration_minutes):
+        start_index = start_minute * 60 * sampling_frequency
+        end_index = (start_minute + duration_minutes) * 60 * sampling_frequency
+        return signal[start_index:end_index]
+
     def opening_operation(signal, distance):
         erosion_result = grey_erosion(signal, size=distance)
         dilation_result = grey_dilation(erosion_result, size=distance)
         return dilation_result
 
-    # Define a function for closing operation
     def closing_operation(signal, distance):
         dilation_result = grey_dilation(signal, size=distance)
         erosion_result = grey_erosion(dilation_result, size=distance)
         return erosion_result
 
-    # Define a function for average OC/CO operation
     def average_occo(signal, distance):
         opening_result = opening_operation(signal, distance)
         closing_result = closing_operation(signal, distance)
         occo_result = (opening_result + closing_result) / 2
         return occo_result
 
-    # Define a function to apply the filter
     def apply_filter(signal, distance, sampling_frequency):
         occo_result = average_occo(signal, distance)
         filtered_signal = signal - occo_result
@@ -39,21 +40,13 @@ def detect_eeg_peaks(signals, start_minute=4, duration_minutes=1):
         signal = signal_data["signal"]
         sampling_frequency = signal_data["samplingFrequency"]
 
-        # Calculate the start and end indices for the specified duration
-        start_index = int(start_minute * 60 * sampling_frequency)
-        end_index = start_index + int(duration_minutes * 60 * sampling_frequency)
+        trimmed_signal = trim_signal(signal, sampling_frequency, start_minute, duration_minutes)
 
-        # Trim the signal to the specified duration
-        trimmed_signal = signal[start_index:end_index]
-
-        # Apply the filter to the trimmed EEG signal
         filtered_signal = apply_filter(trimmed_signal, distance=len(trimmed_signal)//10, sampling_frequency=sampling_frequency)
 
-        # Calculate the threshold
         threshold = 8 * np.median(np.abs(filtered_signal[find_peaks(filtered_signal)[0]]))
 
-        # Count the peaks above the threshold with width and prominence criteria
-        detected_peaks_indices, _ = find_peaks(filtered_signal, height=threshold, width=5, prominence=249)
+        detected_peaks_indices, _ = find_peaks(filtered_signal, height=threshold, width=40, prominence=260)
 
         total_peaks += len(detected_peaks_indices)
 
@@ -71,10 +64,10 @@ if __name__ == "__main__":
             raise ValueError("Missing required fields in input data")
 
         signals = input_data["signals"]
-        start_minute = input_data.get("startMinute", 1)
-        duration_minutes = input_data.get("durationMinutes", 1)
+        start_minute = input_data.get("start_minute", 0)
+        duration_minutes = input_data.get("duration_minutes", 1)
 
-        result = detect_eeg_peaks(signals, start_minute, duration_minutes)
+        result = detect_eeg_peaks(signals, start_minute=start_minute, duration_minutes=duration_minutes)
 
         print(json.dumps(result))
     except Exception as e:
