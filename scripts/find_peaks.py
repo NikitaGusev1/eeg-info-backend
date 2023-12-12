609cc81464cdfd1703742b52bcbded37de9ce8e6
Nikita <nkgs370@gmail.com>
2023-12-12 20:35:04 +0200

    wip

--- scripts/find_peaks.py
+++ scripts/find_peaks.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.signal import find_peaks
 from scipy.ndimage import grey_erosion, grey_dilation
 
-def detect_eeg_peaks(signals, start_minute, duration_minutes=1):
+def detect_eeg_peaks(signals, start_minute, duration_minutes=1, morphological_distance_samples=1280):
     # Function to trim the signal based on start minute and duration
     def trim_signal(signal, sampling_frequency, start_minute, duration_minutes):
         start_index = int(start_minute * 60 * sampling_frequency)
@@ -31,8 +31,8 @@ def detect_eeg_peaks(signals, start_minute, duration_minutes=1):
         return occo_result
 
     # Function to apply the filter and calculate the threshold
-    def apply_filter(signal, distance, sampling_frequency):
-        occo_result = average_occo(signal, distance)
+    def apply_filter(signal, morphological_distance_samples, sampling_frequency):
+        occo_result = average_occo(signal, distance=morphological_distance_samples)
         filtered_signal = np.abs(signal) - np.abs(occo_result)
 
         # Calculate the threshold based on the formula described in the passage
@@ -51,14 +51,13 @@ def detect_eeg_peaks(signals, start_minute, duration_minutes=1):
         sampling_frequency = signal_data["samplingFrequency"]
 
         trimmed_signal = trim_signal(signal, sampling_frequency, start_minute, duration_minutes)
-        filtered_signal, threshold = apply_filter(trimmed_signal, distance=len(trimmed_signal)//10, sampling_frequency=sampling_frequency)
+        filtered_signal, threshold = apply_filter(trimmed_signal, morphological_distance_samples, sampling_frequency)
 
         # Count the peaks above the threshold with width and prominence criteria
-        detected_peaks_indices, _ = find_peaks(filtered_signal, height=threshold, width=50, prominence=60)
+        detected_peaks_indices, _ = find_peaks(filtered_signal, height=threshold, width=5, prominence=30)
 
         total_peaks_per_minute.append({
             "minute": start_minute,
-            # "peaks": detected_peaks_indices.tolist(),
             "total_peaks": len(detected_peaks_indices)
         })
 
@@ -82,7 +81,10 @@ if __name__ == "__main__":
         start_minute = input_data.get("start_minute", 1)
         duration_minutes = input_data.get("duration_minutes", 1)
 
-        result = detect_eeg_peaks(signals, start_minute=start_minute, duration_minutes=duration_minutes)
+        # Set the fixed number of samples for morphological distance
+        morphological_distance_samples = 1280
+
+        result = detect_eeg_peaks(signals, start_minute=start_minute, duration_minutes=duration_minutes, morphological_distance_samples=morphological_distance_samples)
 
         print(json.dumps(result))
     except Exception as e:
